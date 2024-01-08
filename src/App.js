2fb40f759a23469b1e62a50c7aa877cbc0e12c98
Taimoor-Bin-Zahid <taimoorbinzahid86@gmail.com>
2024-01-08 18:12:34 +0500

    Created Folder Structure Added navbar component refactored code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import "./App.css";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './components/navbar';
+
 
 const App = () => {
   return (
     <div className='App'>
       {" "}
       <Router>
+        <Navbar />
         <Routes>
           <Route path='/' />
           <Route path='/cart'/>
